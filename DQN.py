c9f7c4d50822c24efe7f1df9aade20c153023c18
Frequel <frequellfire@gmail.com>
2021-01-21 17:28:22 +0100

    enviroment adaptation for stable baseline
    varios experimentos feitos sobre os codigos (tenteni de adaptar o codigo base , snake-ga) e adaptei o enviroment.py na pasta projecto 2 para ser usado com gm e stable baseline

--- DQN.py
+++ DQN.py
@@ -11,6 +11,7 @@ import copy
 DEVICE = 'cpu' # 'cuda' if torch.cuda.is_available() else 'cpu'
 
 class DQNAgent(torch.nn.Module):
+    #ccercare di integrarlo in Agent.py
     def __init__(self, params):
         super().__init__()
         self.reward = 0
@@ -48,7 +49,7 @@ class DQNAgent(torch.nn.Module):
         x = F.relu(self.f3(x))
         x = F.softmax(self.f4(x), dim=-1)
         return x
-    
+#già c'è in environment.py    
     def get_state(self, game, player, food):
         """
         Return the state.
@@ -105,6 +106,7 @@ class DQNAgent(torch.nn.Module):
 
         return np.asarray(state)
 
+#già c'è in environment.py  
     def set_reward(self, player, crash):
         """
         Return the reward.
---
+++ PROJETO2/experiment.py
@@ -0,0 +1,180 @@
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pygame
+import seaborn as sns
+import random
+
+# our classes
+from agent import Agent, QLearningAgent
+from environment import Environment
+from screen import Screen
+
+import gym
+from stable_baselines3.common.env_checker import check_env
+
+# define environment
+ACTION_SPACE = np.eye(3)
+NUM_ACTIONS = 3
+NUM_STATES = 2 ** 11
+# Set options to activate or deactivate the game view, and its speed
+pygame.font.init()
+def plot_metrics(metrics, filepath=None):
+    formatted_dict = {'episodes': [],
+                      'metrics': [],
+                      'results': []}
+
+    n = len(metrics['episodes'])
+    for i in range(n):
+        episode = metrics['episodes'][i]
+        score = metrics['scores'][i]
+        reward = metrics['rewards'][i]
+
+        formatted_dict['episodes'].append(episode)
+        formatted_dict['metrics'].append('score')
+        formatted_dict['results'].append(score)
+
+        formatted_dict['episodes'].append(episode)
+        formatted_dict['metrics'].append('reward')
+        formatted_dict['results'].append(reward)
+
+    df_metrics = pd.DataFrame(formatted_dict)
+    sns.lineplot(data=df_metrics, x='episodes', y='results', hue='metrics')
+    if filepath is None:
+        plt.show()
+    else:
+        plt.savefig(filepath)
+
+
+def decode_state(encoded_state):
+    """
+    Decode a binary representation of a state into its decimal base;
+    
+    encoded_state: an array of 0s and 1s representing a binary value
+    
+    return: decimal value
+    """
+    decoded = ''
+    for s in encoded_state:
+        decoded += str(s)
+
+    return int(decoded, 2)
+
+
+def decode_action(encoded_action):
+    if isinstance(encoded_action, np.ndarray):
+        return encoded_action.argmax()
+    return encoded_action
+
+def default_reward(env):
+    """
+    Return the reward.
+    The reward is:
+        -10 when Snake crashes.
+        +10 when Snake eats food
+        0 otherwise
+    """
+    reward = 0
+    if env.game.crash:
+        reward = -10
+    elif env.player.eaten:
+        reward = 10
+
+    return reward
+
+def run_q_learning(agent: Agent, reward_function, episodes, display, speed, verbose=True):
+    # setting random seed
+    random.seed(42)
+    np.random.seed(42)
+
+    if display:
+        pygame.init()
+    
+    print('before')
+    env = Environment(440, 440, reward_function)
+    check_env(env, warn=True)
+    print('check_env was ok')
+    screen = Screen(env)
+
+    episode = 0
+    metrics = {'episodes': [],
+               'scores': [],
+               'rewards': []}
+    start = time.time()
+    while episode < episodes:
+        if display:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                    
+            screen.display()
+
+        #print('before')
+        #state1, done = env.reset()
+        state1 = env.reset()
+        done  = env.game.crash
+        #check_env(state1, warn=True)
+        #print('check_env was ok')
+        state1 = decode_state(state1)
+        action1 = agent.choose_action(state1)
+        episode_reward = 0
+        while not done:
+            # Getting the next state, reward
+            state2, reward, done, info = env.step(action1)
+            state2 = decode_state(state2)
+            # Choosing the next action
+            action2 = agent.choose_action(state2)
+
+            # Learning the Q-value
+            decoded_action1 = decode_action(action1)
+            decoded_action2 = decode_action(action2)
+            agent.update(state1, state2, reward, decoded_action1, decoded_action2)
+
+            state1 = state2
+            action1 = action2
+            episode_reward += reward
+
+            if display:
+                screen.display()
+                pygame.time.wait(speed)
+            
+            end = time.time()
+            diff = end - start
+            if diff > 600: # 10min
+                break
+
+        episode += 1
+        if verbose:
+            print(f'Game {episode}      Score: {env.game.score}')
+
+        mean_reward = episode_reward/episodes
+        metrics['episodes'].append(episode)
+        metrics['rewards'].append(mean_reward)
+        metrics['scores'].append(env.game.score)
+        
+        end = time.time()
+        diff = end - start
+        if diff > 600: # 10min
+            break
+        
+
+    return metrics
+
+N0 = 1
+gamma = 1
+
+# define agent
+qLearningAgent = QLearningAgent(N0, gamma, NUM_STATES, NUM_ACTIONS, ACTION_SPACE)
+
+start = time.time()
+metrics = run_q_learning(qLearningAgent, reward_function=default_reward, episodes=1000, speed=0, display=False)
+end = time.time()
+
+plot_metrics(metrics, filepath=None)
+
+print('Run time:', (end-start), 'seconds')
+print('Max. Score:', max(metrics['scores']))
+print('Mean Last Scores:', np.mean(metrics['scores'][-50:]))
